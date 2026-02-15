8e78df1c9e8f891804edada3ddbc0eda1157a14a
hardik-pattern <hardik.jade+github@gmail.com>
2026-02-16 00:10:39 +0530

    Add .DS_Store files and update main.go in learning-go module
    Introduced .DS_Store files for project directories and modified the main.go file in the 01hello module to replace the original greeting with a formatted welcome message that includes a name and age variable.

--- learning-go/01hello/main.go
+++ learning-go/01hello/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from hardik")
+	// fmt.Println("Hello from hardik")
+	name := "Hardik"
+	age := 010
+
+	fmt.Println(fmt.Sprintf("Welcome to my party, %s %d", name, age))
 }
